b8248d227a3202cd6c80d3a568470486c3ee33c2
huy le <lqh1411@gmail.com>
2023-09-29 19:00:31 +0700

    udpate scripts

--- scripts/run_solution.sh
+++ scripts/run_solution.sh
@@ -6,10 +6,23 @@ orange=$(tput setaf 178)
 bold=$(tput bold)
 reset=$(tput sgr0)
 
+type="cpp"
 target=$1
+type="$2"
 
-echo "${green}Input:${reset}"
-./${target} >output
-echo "${green}Output:${reset}"
+if [ "$type" = "cpp" ]; then
+	echo "${green}Input:${reset}"
+	./${target} >output
+	echo "${green}Output:${reset}"
+elif [ "$type" = "python" ]; then
+	echo $target
+	echo "${green}Input:${reset}"
+	python3 -W default ${target}.py >output
+	echo "${green}Output:${reset}"
+else
+	echo "default ....."
+	echo $type
+fi
+
 cat output
 rm output
