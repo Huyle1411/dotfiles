830baf983db1e0c64f8bd73a9ccc2b0a83af35c6
huy le <lqh1411@gmail.com>
2023-10-22 10:38:06 +0700

    update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,7 @@ local opts = { silent = true }
 -- vim.opt.makeprg = "cd build && time make"
 keymap("n", "<F5>", ":%y+ <CR>", opts)
 keymap("i", "<F5>", "<ESC> :%y+ <CR>", opts)
+keymap("n", "<F9>", ':w <bar> :TermExec cmd="dbrun %:r" <CR>', opts)
+keymap("i", "<F9>", '<ESC> :w <bar> :TermExec cmd="dbrun %:r" <CR>', opts)
+keymap("n", "<F10>", ':w <bar> :TermExec cmd="runsp %:r" <CR>', opts)
+keymap("i", "<F10>", '<ESC> :w <bar> :TermExec cmd="runsp %:r" <CR>', opts)
