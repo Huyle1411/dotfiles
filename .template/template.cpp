257935f5b1c5122b5d8f95ad3b98198845fec4d8
huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2024-05-02 10:26:11 +0700

    chore: add template for cpp extension, same content with cc file

---
+++ .template/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+#ifdef DEBUG
+#include "debugutil.h"
+#else
+#define debug(...) 666
+#define debugArr(arr, n) 666
+#endif
+
+int32_t main() {
+    std::cin.tie(0)->sync_with_stdio(0);
+    std::cin.exceptions(std::cin.failbit);
+    int tt = 1;
+    std::cin >> tt;
+    while (tt--) {
+    }
+}
