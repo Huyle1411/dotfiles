f34502fb832d735a36e8fac10851ed32c8a9f78b
HuyLe Personal Laptop <lqh1411@gmail.com>
2024-05-05 15:24:27 +0700

    fix: change template code python

--- .template/template.py
+++ .template/template.py
@@ -3,6 +3,11 @@ import math
 import itertools
 import functools
 import collections
+
 input = sys.stdin.readline
 
+output_ans = []
 for _ in range(int(input())):
+    pass
+
+print(*output_ans, sep="\n")
